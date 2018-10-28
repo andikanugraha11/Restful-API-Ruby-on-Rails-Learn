934799552f1f1f75184e2a9e10cce0d2e66350fc
Andika Nugraha <m.andika.nugraha@gmail.com>
2018-10-28 23:12:36 +0700

    Update, post delete

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,21 @@
 class HomeController < ApplicationController
+    # # Untuk skip check authentication semacam CSRF Token di Laravel
+    # skip_before_filter :verify_authenticity_token
+    # # Handling ketika data not found saat update, dan delete
+    rescue_from ActiveRecord::RecordNotFound, with: :notFound
 
+
+    def userParams
+        params.permit(:name,:age)
+    end
+
+    def notFound
+        render json:{
+            values:{},
+            massage:"Data not found",
+        },status:400
+    end
+        
     def index
         @user = User.all
         render json: {
@@ -23,4 +39,48 @@ class HomeController < ApplicationController
         end
     end
 
+    def create
+        @user = User.new(userParams)
+        if @user.save
+            render json:{
+                values: @user,
+                message: "success",
+            },status: 200
+        else
+            render json:{
+            values: "",
+            message: "Save failed",
+            },status: 400
+        end
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update(userParams)
+            render json:{
+                values: @user,
+                message: "success",
+            },status: 200
+        else
+            render json:{
+            values: "",
+            message: "Update failed",
+            },status: 400
+        end
+    end
+
+    def destroy
+        @user = User.find(params[:id])
+        if @user.destroy
+            render json:{
+                values: @user,
+                message: "success",
+            },status: 200
+        else
+            render json:{
+            values: "",
+            message: "Save failed",
+            },status: 400
+        end
+    end
 end
